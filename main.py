a3031bf5dcdb8c8147d530afb3a23e10c534e647
Giovanne de Luna <gikalunalticg@gmail.com>
2024-02-19 12:56:43 -0300

    add get and post

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from product import Product
+from json_db import JsonDB
+
+
+app = FastAPI()
+
+productDB = JsonDB(path="./data/products.json")
+
+@app.get('/products')
+def get_products():
+    products = productDB.read()
+    return {"products": products}
+
+
+@app.post('/products')
+def create_product(product: Product):
+    productDB.insert(product)
+    return {"status": "inserted"}
